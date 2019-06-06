bb44e825e2ef785ea32cd8d8467c851668d5b480
Heather Kirk <hskirk@gmail.com>
2019-06-06 00:34:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,9 @@ class PostsController < ApplicationController
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
-
+	
+  private
+  
 	def edit
 	  @post = Post.find(params[:id])
 	end
